5eb4b14392ae0e56e0114a85a857f450017e2f01
Dawid Marcinków <dawid.marcinkow@gmail.com>
2018-12-22 19:56:23 +0100

    Working with invoice template in jasper

--- src/main/java/pl/invoicemanager/InvoiceManager/controller/InvoiceController.java
+++ src/main/java/pl/invoicemanager/InvoiceManager/controller/InvoiceController.java
@@ -6,14 +6,16 @@ import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.invoicemanager.InvoiceManager.dao.CompanyDao;
 import pl.invoicemanager.InvoiceManager.dao.InvoiceDao;
+import pl.invoicemanager.InvoiceManager.models.Company;
 import pl.invoicemanager.InvoiceManager.models.Invoice;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
-import java.util.Random;
+import java.time.LocalDateTime;
+import java.util.*;
 
 @Controller
 @RequestMapping("/invoice")
@@ -24,6 +26,9 @@ public class InvoiceController {
 
 	@Autowired
 	InvoiceDao invoiceDao;
+
+	@Autowired
+	CompanyDao companyDao;
 
 	@RequestMapping("/add")
 	public String addInvoice(){
@@ -45,10 +50,27 @@ public class InvoiceController {
 
 	@RequestMapping("/generate")
 	public void generateInvoice(HttpServletResponse response) throws IOException, JRException {
-		String path = resourceLoader.getResource("classpath:/reports/invoice.jrxml").getURI().getPath();
+//		String path = resourceLoader.getResource("classpath:/reports/invoice.jrxml").getURI().getPath();
+		String path = resourceLoader.getResource("classpath:/reports/PaidInvoice.jrxml").getURI().getPath();
 		JasperReport jasperReport = JasperCompileManager.compileReport(path);
 		JRDataSource jrDataSource = new JREmptyDataSource();
-		JasperPrint print = JasperFillManager.fillReport(jasperReport,null, jrDataSource);
+/////////////////////////////
+		Invoice invoice = new Invoice();
+		invoice.setInvoiceNumber("1/01/2019");
+		invoice.setCreateDate(new Date());
+		invoice.setSoldDate(new Date());
+		invoice.setPaidDate(new Date());
+		invoice.setPaymentType("gotówka");
+		List<Company> companies = companyDao.findAll();
+		Company company = companies.get(0);
+		Company company2 = companies.get(1);
+
+		////////////////////////////
+		Map<String,Object> parameters = new HashMap<>();
+		parameters.put("invoice",invoice);
+		parameters.put("seller", company);
+		parameters.put("buyer", company2);
+		JasperPrint print = JasperFillManager.fillReport(jasperReport,parameters, jrDataSource);
 		response.setContentType("application/pdf");
 		response.setHeader("Content-Disposition",String.format("attachment; filename=\"faktura.pdf\""));
 		ServletOutputStream servletOutputStream = response.getOutputStream();
--- src/main/java/pl/invoicemanager/InvoiceManager/models/Invoice.java
+++ src/main/java/pl/invoicemanager/InvoiceManager/models/Invoice.java
@@ -1,6 +1,7 @@
 package pl.invoicemanager.InvoiceManager.models;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "invoice")
@@ -14,6 +15,18 @@ public class Invoice {
 	@Column(name = "INVOICE_NO", nullable = false, unique = true)
 	private String invoiceNumber;
 
+	@Column(name = "INVOICE_SOLD_DATE")
+	private Date soldDate;
+
+	@Column(name = "INVOICE_CREATE_DATE")
+	private Date createDate;
+
+	@Column(name = "INVOICE_PAID_DATE")
+	private Date paidDate;
+
+	@Column(name = "INVOICE_PAYMENT_TYPE")
+	private String paymentType;
+
 	@ManyToOne
 	@JoinColumn(name = "COMPANY_ID", nullable = false)
 	private Company company;
@@ -24,6 +37,46 @@ public class Invoice {
 
 	public void setInvoiceNumber(String invoiceNumber) {
 		this.invoiceNumber = invoiceNumber;
+	}
+
+	public Date getSoldDate() {
+		return soldDate;
+	}
+
+	public void setSoldDate(Date soldDate) {
+		this.soldDate = soldDate;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public Date getPaidDate() {
+		return paidDate;
+	}
+
+	public void setPaidDate(Date paidDate) {
+		this.paidDate = paidDate;
+	}
+
+	public String getPaymentType() {
+		return paymentType;
+	}
+
+	public void setPaymentType(String paymentType) {
+		this.paymentType = paymentType;
+	}
+
+	public Company getCompany() {
+		return company;
+	}
+
+	public void setCompany(Company company) {
+		this.company = company;
 	}
 
 	@Override
